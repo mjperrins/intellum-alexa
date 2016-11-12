240147c187b02ce8c614b47529739b66c74b87f8
Matthew Perrins <mjperrin@us.ibm.com>
2016-11-12 15:52:43 -0600

    Fix package.json

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 'use strict';
 
-var LOCAL=false;
-
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-var appEnv = cfenv.getAppEnv()
+var appEnv = cfenv.getAppEnv();
 
 // Load Alex App Server
 var AlexaAppServer = require( 'alexa-app-server' );
 
 if (appEnv.isLocal) {
+	console.log("LOCAL");
 	var server = new AlexaAppServer( {
 		httpsEnabled: false,
 		port: appEnv.port
 	} );
 } else {
-
+	console.log("REMOTE");
 	var server = AlexaAppServer( 
 		{
-			port: appEnv.port,
+			port : appEnv.port,
 			httpsEnabled:true,
 			privateKey:'private-key.pem',
 			certificate:'cert.cer'
